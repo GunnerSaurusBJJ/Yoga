c5b067959f3091cbbe048809584298f967018766
GunnerSaurusBJJ <ddaa.99ars@yandex.ru>
2020-01-30 14:46:15 +0300

    yoga

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".slider__exp").slick({
+    arrows: false,
+    slidesToShow: 1,
+    centerMode: true,
+    slidesToScroll: 1,
+    dots: false,
+
+    variableWidth: true
+  });
+});
